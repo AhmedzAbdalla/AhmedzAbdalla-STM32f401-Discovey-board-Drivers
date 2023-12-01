ff20134434df6cd62890672f40223300037011ec
Ahmed Abdalla <ahmed.m.abdalla650@gmail.com>
2023-12-01 23:48:22 +0200

    v1.0

--- Inc/LIB/BIT_MATH.h
+++ Inc/LIB/BIT_MATH.h
@@ -3,13 +3,19 @@
  * @file           : BIT_MATH.h
  * @author         : Ahmed Abdalla
  * @brief          : this file contains some bitmath operation
- * @date		   : 16 Dec 2023 
+ * @date		   : 16 Nov 2023 
  ******************************************************************************
  */
 
 #ifndef HEADER_FILES_BIT_MATH_H_
 #define HEADER_FILES_BIT_MATH_H_
 
+#define BIT_MASK 1UL
+
+#define SET_BIT(REG , POS) 	 	(REG|=(1<<POS))
+#define CLR_BIT(REG , POS)  	(REG&=(~(1<<POS)))
+#define Toggle_BIT(REG , POS)   (REG^=(1<<POS))
+#define READ_BIT(REG , POS) 	((REG>>POS) & BIT_MASK)
 #define BIT_MASK 1UL
 
 #define SET_BIT(REG , POS) 	 	(REG|=(1<<POS))
